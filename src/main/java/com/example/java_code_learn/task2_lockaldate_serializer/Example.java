e17e6fafc33ddbd85c58b0e453e11385701235d2
kolia <koliamba_85@mail.ru>
2024-12-03 21:42:22 +0800

    Task complete

--- src/main/java/com/example/java_code_learn/task2_lockaldate_serializer/Example.java
+++ src/main/java/com/example/java_code_learn/task2_lockaldate_serializer/Example.java
@@ -1,27 +1,23 @@
 package com.example.java_code_learn.task2_lockaldate_serializer;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import java.time.LocalDateTime;
 
 public class Example {
-    //С сериалайзером не работает
-    //@JsonSerialize(using = MyDateSerializer.class)
-
-    //С этой аннотацией так же не работает
-    @JsonFormat(pattern = "yyyy:MM:dd##:HH:mm:ss:SSS")
+    @JsonFormat(shape = JsonFormat.Shape.STRING,
+            pattern = "yyyy:MM:dd :HH:mm:ss:SSS",
+            locale = "ru")
     private LocalDateTime date;
 
-    public Example() {
+    public Example(LocalDateTime date) {
+        this.date = date;
     }
 
     public void setDate(LocalDateTime date) {
         this.date = date;
     }
 
-    //Пробовал на геттер повешать аннотации, тоже не получилось
-    //@JsonSerialize(using = MyDateSerializer.class)
     public LocalDateTime getDate() {
         return date;
     }
--- src/main/java/com/example/java_code_learn/task2_lockaldate_serializer/Main.java
+++ src/main/java/com/example/java_code_learn/task2_lockaldate_serializer/Main.java
@@ -1,19 +1,21 @@
 package com.example.java_code_learn.task2_lockaldate_serializer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class Main {
-    public static void main(String[] args) {
-        Example example = new Example();
-
+    public static void main(String[] args) throws Exception {
+        /*Example example = new Example();
         example.setDate(LocalDateTime.now());
 
-        //С паттерном работает, если убрать ##
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy:MM:dd##:HH:mm:ss:SSS");
-        System.out.println(example.getDate().format(formatter));
-
-        //Тут отображается в формате по умолчанию
-        System.out.println(example.getDate());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy:MM:dd :HH:mm:ss:SSS");
+        System.out.println(example.getDate().format(formatter));*/
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        Example example = new Example(LocalDateTime.now());
+        String jsonString = mapper.writeValueAsString(example);
+        System.out.println(jsonString);
     }
 }
